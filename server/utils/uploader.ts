83e037a1187aa12350ad89b7db1455c982a90b3b
youpps <riashenka@gmail.com>
2023-11-13 20:12:06 +0300

    some fixes

--- server/utils/uploader.ts
+++ server/utils/uploader.ts
@@ -18,6 +18,7 @@ class Uploader {
         imagemagick.resize(
           {
             srcPath: uploadPath,
+            dstPath: uploadPath,
             width,
             height,
           },
