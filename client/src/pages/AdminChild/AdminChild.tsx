cc1fdf57a0a4e47e4f1c80dad04a839816d2e205
youpps <riashenka@gmail.com>
2023-11-17 14:56:47 +0300

    some fixes

--- client/src/pages/AdminChild/AdminChild.tsx
+++ client/src/pages/AdminChild/AdminChild.tsx
@@ -91,6 +91,59 @@ const AdminChild = () => {
     return true;
   };
 
+  const resizeImage = (file: File, maxSize: number): Promise<Blob> => {
+    var reader = new FileReader();
+    var image = new Image();
+
+    var canvas = document.createElement("canvas");
+
+    const dataURItoBlob = (dataURI: string) => {
+      const bytes = dataURI.split(",")[0].indexOf("base64") >= 0 ? atob(dataURI.split(",")[1]) : unescape(dataURI.split(",")[1]);
+      const mime = dataURI.split(",")[0].split(":")[1].split(";")[0];
+      const max = bytes.length;
+      const ia = new Uint8Array(max);
+      for (var i = 0; i < max; i++) ia[i] = bytes.charCodeAt(i);
+      return new Blob([ia], { type: mime });
+    };
+
+    const resize = () => {
+      let width = image.width;
+      let height = image.height;
+      if (width > height) {
+        if (width > maxSize) {
+          height *= maxSize / width;
+          width = maxSize;
+        }
+      } else {
+        if (height > maxSize) {
+          width *= maxSize / height;
+          height = maxSize;
+        }
+      }
+      canvas.width = width;
+      canvas.height = height;
+      canvas.getContext("2d")?.drawImage(image, 0, 0, width, height);
+      const dataUrl = canvas.toDataURL("image/jpeg");
+      return dataURItoBlob(dataUrl);
+    };
+
+    return new Promise(function (ok, no) {
+      if (!file.type.match(/image.*/)) {
+        no(new Error("Not an image"));
+        return;
+      }
+
+      reader.onload = function (readerEvent) {
+        image.onload = function () {
+          return ok(resize());
+        };
+        image.src = (readerEvent.target?.result ?? "") as string;
+      };
+
+      reader.readAsDataURL(file);
+    });
+  };
+
   async function onAvatarChange(e: React.ChangeEvent<HTMLInputElement>) {
     e.preventDefault();
 
@@ -108,7 +161,9 @@ const AdminChild = () => {
 
     const formData = new FormData();
 
-    formData.append("avatar", file);
+    const correctImage = (await resizeImage(file, 1024)) as any;
+
+    formData.append("avatar", correctImage);
 
     const res = await AdminChildrenService.changeAvatar(child.id, formData);
     if (res.status === "success") {
@@ -160,7 +215,9 @@ const AdminChild = () => {
 
     const formData = new FormData();
 
-    formData.append("image", file);
+    const correctImage = (await resizeImage(file, 1024)) as any;
+
+    formData.append("image", correctImage);
 
     const res = await AdminChildrenService.addImage(child.id, formData);
     if (res.status === "success") {
