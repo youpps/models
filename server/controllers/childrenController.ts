adac7dcffefb7b95da110b45e4137e2945a0c57d
youpps <riashenka@gmail.com>
2023-11-17 16:26:06 +0300

    some fixes

--- server/controllers/childrenController.ts
+++ server/controllers/childrenController.ts
@@ -49,28 +49,44 @@ class ChildrenController {
 
   getChildrenFilters = async (req: Request, res: Response) => {
     try {
-      const specialization = await this.repositories.childrenRepository.getChildrenFilter("specialization");
-      const sex = await this.repositories.childrenRepository.getChildrenFilter("sex");
-      const age = await this.repositories.childrenRepository.getChildrenFilter("birthDate");
-      const height = await this.repositories.childrenRepository.getChildrenFilter("height");
-      const hairColor = await this.repositories.childrenRepository.getChildrenFilter("hairColor");
-      const eyeColor = await this.repositories.childrenRepository.getChildrenFilter("eyeColor");
-      const shoesSize = await this.repositories.childrenRepository.getChildrenFilter("shoesSize");
-      const city = await this.repositories.childrenRepository.getChildrenFilter("city");
-      const video = await this.repositories.childrenRepository.getChildrenFilter("video");
+      const filtersPairs = {
+        specialization: "specialization",
+        sex: "sex",
+        age: "birthDate",
+        height: "height",
+        hairColor: "hairColor",
+        eyeColor: "eyeColor",
+        shoesSize: "shoesSize",
+        city: "city",
+        video: "video",
+      };
+
+      const pool = [];
+      const filters: { [key: string]: ChildrenFilter[] } = {};
+
+      for (let filter in filtersPairs) {
+        pool.push(async () => {
+          const sqlKey: any = (filtersPairs as any)[filter];
+          filters[filter] = await this.repositories.childrenRepository.getChildrenFilter(sqlKey);
+        });
+      }
+
+      await Promise.allSettled(pool);
+
+      // const specialization = await this.repositories.childrenRepository.getChildrenFilter("specialization");
+      // const sex = await this.repositories.childrenRepository.getChildrenFilter("sex");
+      // const age = await this.repositories.childrenRepository.getChildrenFilter("birthDate");
+      // const height = await this.repositories.childrenRepository.getChildrenFilter("height");
+      // const hairColor = await this.repositories.childrenRepository.getChildrenFilter("hairColor");
+      // const eyeColor = await this.repositories.childrenRepository.getChildrenFilter("eyeColor");
+      // const shoesSize = await this.repositories.childrenRepository.getChildrenFilter("shoesSize");
+      // const city = await this.repositories.childrenRepository.getChildrenFilter("city");
+      // const video = await this.repositories.childrenRepository.getChildrenFilter("video");
 
       return res.status(200).json({
         status: Status.Success,
         data: {
-          specialization,
-          sex,
-          age,
-          height,
-          hairColor,
-          eyeColor,
-          shoesSize,
-          city,
-          video,
+          ...filters,
         },
       });
     } catch (e) {
